67dc3b8e71bf85f8a884f8294fe1943e87929f1f
cotool <tusky.saga@gmail.com>
2020-08-25 15:54:04 +0800

    feat:add user authentication

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
 const users = require('./modules/users')
 
+const { authenticator } = require('../middleware/auth')
+
 
 // route setting
-router.use('/restaurant', restaurant)
+router.use('/restaurant',authenticator, restaurant)
 router.use('/users', users)
-router.use('/', home)
+router.use('/',authenticator, home)
 
 
 module.exports = router
